c5941fcf336bd78e06f816fced1f5fb83c20c0b0
wareez92 <wareezsowemimo@gmail.com>
2024-07-30 11:17:33 -0500

    border removed | margins adjusted | bounce animation created

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,15 @@
+
 const Card = ({ data }) => {
   return (
-    <div key={data.id}>
+    <div className="poke-container" key={data.id}>
       {data.map((item) => (
-        <div key={data.id}>
+        <div className="pokecard" key={data.id}>
           <h3>{item.pokemon}</h3>
           <h6>HP {item.hitpoints}</h6>
           <h6>{item.type}</h6>
           <img src={item.image_url} alt={item.movie} />
           <h6>{item.abilities}</h6>
-          <h6>{item.evolutions}</h6>
+          {/* <h6>{item.evolutions}</h6> */}
           <h6>{item.location}</h6>
         </div>
       ))}
